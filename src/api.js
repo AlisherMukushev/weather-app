4a921bbdca1366773b013188959dfa87f7b4986f
Alisher Mukushev <mukushev.alisher94@gmail.com>
2022-11-16 13:35:33 +0600

    start weather app

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': 'bb4c0c23bdmshb516f4b3d8362dap1c9505jsn6bc44867b6a6',
+    'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+  },
+}
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo'
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5'
+
+export const WEATHER_API_KEY = '6cb7d2edeb1602c466f5b4ef2a8aacb0'
